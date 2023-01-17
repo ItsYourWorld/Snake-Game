9948c69a73dfcbe8962661ad2e218bee30291113
ItsYourWorld <118554763+ItsYourWorld@users.noreply.github.com>
2023-01-17 10:42:27 +0200

    Update game.js

--- game.js
+++ game.js
@@ -27,13 +27,13 @@ document.addEventListener("keydown", direction);
 let dir;
 
 function direction(event) {
-    if(event.keyCode == 37 && dir != "right")
+    if(event.keyCode == 65 && dir != "right")
         dir = "left";
-    else if(event.keyCode == 38 && dir != "down")
+    else if(event.keyCode == 83 && dir != "down")
         dir = "up";
-    else if(event.keyCode == 39 && dir != "left")
+    else if(event.keyCode == 68 && dir != "left")
         dir = "right";
-    else if(event.keyCode == 40 && dir != "up")
+    else if(event.keyCode == 87 && dir != "up")
         dir = "down";
 }
 
